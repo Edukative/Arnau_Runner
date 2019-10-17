cce10efe708cd290fd12acdf4e60d3f3e930ff37
Edukative <56083723+Edukative@users.noreply.github.com>
2019-10-17 19:33:26 +0200

    Start New Project

---
+++ Arnau_Runner/Assets/Scripts/Player_Controler.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Controler : MonoBehaviour
+{
+    private Rigidbody playerRB;
+
+    public float jumpForce;
+    public float GravityModifier;
+
+    public bool IsOnGround = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRB = GetComponent<Rigidbody>();
+        playerRB.AddForce(Vector3.up * 500);
+
+        Physics.gravity *= GravityModifier;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      if (Input.GetKeyDown(KeyCode.Space) && IsOnGround)
+        {
+            playerRB.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            IsOnGround = false;
+        } 
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        IsOnGround = true;
+    }
+
+}
---
+++ Arnau_Runner/Assets/Scripts/Spanw_Manager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spanw_Manager : MonoBehaviour
+{
+    public GameObject obstalePrefab;
+    private Vector3 spawnPos = new Vector3(35, 0, 0);
+
+    private float startDelay = 2;
+    private float repeatRate = 2;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnObstacle", startDelay, repeatRate);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SpawnObstacle()
+    {
+        Instantiate(obstalePrefab, spawnPos, obstalePrefab.transform.rotation);
+    }
+}
